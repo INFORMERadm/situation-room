d4bb949be472f23bf2d9e1b2f5369a0df9a93b8d
INFORMERadm <broukpitlik@gmail.com>
2026-02-11 19:26:37 +0400

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -40,7 +40,7 @@ export default function Header() {
           <path d="M2 12h20" />
           <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z" />
         </svg>
-        GLOBAL MONITOR
+        <span style={{ position: 'relative', top: 2 }}>GLOBAL MONITOR</span>
       </div>
       <WorldClocks />
     </div>
