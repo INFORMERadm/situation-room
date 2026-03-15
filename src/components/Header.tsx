6eadcbc5723e9d99e46b8ac631ef25ffc25bf062
INFORMERadm <broukpitlik@gmail.com>
2026-03-16 00:34:11 +0400

    Update header logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -36,7 +36,7 @@ export default function Header({ externalClocks, onAddClock, onRemoveClock, onMC
         }}
       >
         <img
-          src="/noBgBlack_(3).png"
+          src="/noBgBlack_(4).png"
           alt="N4-AI Logo"
           style={{ height: 32, objectFit: 'contain' }}
         />
