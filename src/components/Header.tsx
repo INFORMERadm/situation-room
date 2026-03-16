dc1dd2311a49f201c8434f7de301987a6e11ccaa
INFORMERadm <broukpitlik@gmail.com>
2026-03-16 17:49:50 +0400

    Update logo size to 48px

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,7 +38,7 @@ export default function Header({ externalClocks, onAddClock, onRemoveClock, onMC
         <img
           src="/Factonaut_Logo_2.png"
           alt="Echo Logo"
-          style={{ height: 36, objectFit: 'contain', marginLeft: -8 }}
+          style={{ height: 48, objectFit: 'contain', marginLeft: -8 }}
         />
       </div>
       <div style={{ display: 'flex', alignItems: 'center', gap: 12 }}>
