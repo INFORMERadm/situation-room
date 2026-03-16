6982a6467304d99f8d43f757bde032237f33b9fc
INFORMERadm <broukpitlik@gmail.com>
2026-03-16 22:19:19 +0400

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,7 +38,7 @@ export default function Header({ externalClocks, onAddClock, onRemoveClock, onMC
         <img
           src="/noBgColor_white_yellow2.png"
           alt="N4 Logo"
-          style={{ height: 28, objectFit: 'contain', marginLeft: -8 }}
+          style={{ height: 40, objectFit: 'contain', marginLeft: -8 }}
         />
         <span style={{
           fontSize: 10,
