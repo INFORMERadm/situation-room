af0a4f58213f75ed0519958d3527c508b969e58a
INFORMERadm <broukpitlik@gmail.com>
2026-03-27 15:01:59 +0400

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -35,7 +35,7 @@ export default function Header({ externalClocks, onAddClock, onRemoveClock, onMC
           whiteSpace: 'nowrap',
         }}
       >
-        <img src="/noBgWhite_(2).png" alt="N4" style={{ height: 46, marginRight: 10, marginLeft: -3, flexShrink: 0, objectFit: 'contain', imageRendering: 'auto', WebkitFontSmoothing: 'antialiased' }} />
+        <img src="/noBgWhite_new_full.png" alt="N4" style={{ height: 36, marginRight: 10, marginLeft: -3, flexShrink: 0, objectFit: 'contain', imageRendering: 'auto', WebkitFontSmoothing: 'antialiased' }} />
         <span style={{
           fontSize: 10,
           fontWeight: 500,
@@ -43,7 +43,7 @@ export default function Header({ externalClocks, onAddClock, onRemoveClock, onMC
           textTransform: 'uppercase',
           color: 'rgba(255,255,255,0.5)',
           position: 'relative',
-          top: 5,
+          top: 9,
           marginLeft: 4,
         }}>Professional Version</span>
       </div>
