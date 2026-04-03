28a5c8c0f1bb08cd22b074392aa31cd3febc0c1d
INFORMERadm <broukpitlik@gmail.com>
2026-04-03 13:19:57 +0400

    Move slogan up 1px

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -43,7 +43,7 @@ export default function Header({ externalClocks, onAddClock, onRemoveClock, onMC
           textTransform: 'uppercase',
           color: 'rgba(255,255,255,0.5)',
           position: 'relative',
-          top: 3,
+          top: 2,
           marginLeft: 4,
         }}>AI for Managers</span>
       </div>
